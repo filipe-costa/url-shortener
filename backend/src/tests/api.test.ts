dbbbee53b6429af5fa7f0841d3ae13f4d940ee56
Filipe Costa <filipegraca1992@gmail.com>
2021-07-20 18:11:51 +0100

    Move tests to a single module in order to reuse the database connection

---
+++ backend/src/tests/api.test.ts
@@ -0,0 +1,106 @@
+import { Connection, Repository, createConnection, getConnection} from "typeorm";
+import axios from "axios"
+import dotenv from "dotenv"
+import { Server } from "http"
+import path from "path"
+
+import { Url, UrlEntity } from "../db/entity/Url";
+import server from "../server"
+
+dotenv.config({path: path.join(__dirname, "../../", "test.env")})
+
+describe("API Testing module", () => {
+  const BASE_URL = `http://localhost:${process.env.HTTP_PORT}`
+  let app: Server
+  let connection: Connection
+  let URLRepository: Repository<Url>
+
+  beforeAll(async() => {
+    // App will create a connection for us
+    app = await server() as Server
+    connection = await getConnection()
+    // Get url repository
+    URLRepository = await connection.getRepository(UrlEntity)
+    // Drop database and create new one
+    await connection.synchronize(true)
+  })
+  
+  afterAll(async() => {
+    // Close connections
+    await connection.dropDatabase()
+    await connection.close()
+    await app.close()
+  })
+
+  describe("API tests", () => {
+    beforeAll(async() => {
+      await connection.synchronize(true)
+    })
+    it("creates a new url and returns its hash", async() => {
+      const res = await axios.post(`${BASE_URL}/urls`, {url: `${BASE_URL}/healthz`})
+      expect(res.status).toBe(200)
+      expect(res.data.url).toEqual(`${BASE_URL}/jR`)
+    })
+  
+    it("gets url using a hash", async() => {
+      const res = await axios.get(`${BASE_URL}/jR`)
+      expect(res.status).toBe(200)
+    })
+  
+    it("throws error when using unexistent hash", async() => {
+      try {
+        await axios.get(`${BASE_URL}/foo`)
+      } catch(err) {
+        if(err.response) {
+          expect(err.response.status).toBe(404)
+        }
+      }
+    })
+  })
+
+  describe("Database tests", () => {
+    beforeAll(async() => {
+      await connection.synchronize(true)
+    })
+
+    afterEach(async() => {
+      await URLRepository.clear()
+    })
+
+    it("inserts a new url", async() => {
+      const mockURL = {
+        id: 1,
+        url: "https://google.com"
+      }
+      await URLRepository.insert({url: "https://google.com"})
+      const url = await URLRepository.findOne()
+      expect(url).toEqual(mockURL)
+    })
+  
+    it("inserts two new urls", async() => {
+      const mockURLs = [
+        {
+          id: 1,
+          url: "https://google.com"
+        },
+        {
+          id: 2,
+          url: "https://news.ycombinator.com/"
+        }
+      ]
+  
+      await URLRepository.insert({url: "https://google.com"})
+      await URLRepository.insert({url: "https://news.ycombinator.com/"})
+  
+      const urls = await URLRepository.findAndCount()
+  
+      expect(urls[1]).toEqual(2)
+      expect(urls[0][0].url).toEqual(mockURLs[0].url)
+      expect(urls[0][1].url).toEqual(mockURLs[1].url)
+    })
+  })
+})
+
+
+
+
