69cd3dd90df268ecfb48765850444578cc3a87b6
Filipe Costa <filipegraca1992@gmail.com>
2021-07-25 22:25:50 +0100

    eject react app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,23 @@
-import React, {FC} from 'react';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App: FC = () => {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Learn React
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
